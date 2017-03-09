a8dcd76f1f429a33e8804488616c8102c91596ef
Timurgorez <timuumit@mail.ru>
2017-03-09 23:56:59 +0200

    Revert "first commit"
    This reverts commit 22f31a7910e13a82b9557d7642330ad0bbf8390f.

--- js/PopUp.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    PopUpHide();
-});
-function PopUpShow(){
-    $("#popup1").show();
-}
-function PopUpHide(){
-    $("#popup1").hide();
-}
--- js/main.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-	$("#main > ul > li > a").click(function() {
-		$(this).find("~ ul").stop().slideToggle("slov");
-	});
-});
